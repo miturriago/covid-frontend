96093cdc36f56b6bc53408df1b7bdd0b06049cb2
Mauricio Maldonado Iturriago <mauriciomaldo@hotmail.es>
2020-04-12 12:56:39 -0500

    token updated

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ firebase.initializeApp(firebaseConfig);
 Vue.prototype.$http = Axios;
 const token = localStorage.getItem('token')
 if (token) {
-  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+  Vue.prototype.$http.defaults.headers.common['Authorization'] =  'Token '+token
 }
 
 // Vue.use(VueGoogleMaps, {
